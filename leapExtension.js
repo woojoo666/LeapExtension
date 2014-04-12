425e649df4a2f49219682c02ca227ba92f15c5d5
woojoo666 <maxithewoowoo@gmail.com>
2014-04-12 14:51:50 -0700

    keypress -> keyup and keydown

--- leapExtension.js
+++ leapExtension.js
@@ -1,5 +1,16 @@
-var simulateKeyPress = function (character) {
-    console.log('Leap gesture: ' + (['left','up','right','down'])[character-37]);
+var simulateKeyPress = function(character) {
+    console.log('Leap gesture: ' + (['left', 'up', 'right', 'down'])[character - 37]);
+    var eventObj = document.createEvent("Events");
+    eventObj.initEvent("keydown", true, true);
+    eventObj.which = character;
+    (document.activeElement || document.body).dispatchEvent(eventObj);
+    setTimeout(function() {
+        simulateKeyUp(character);
+    }, 50);
+};
+
+var simulateKeyUp = function(character) {
+    console.log('Leap gesture: ' + (['left', 'up', 'right', 'down'])[character - 37]);
     var eventObj = document.createEvent("Events");
     eventObj.initEvent("keypress", true, true);
     eventObj.which = character;
